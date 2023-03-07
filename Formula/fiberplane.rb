1a96e5f3c427f49ade8cf6dd48a41e376fd3b81c
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-03-07 14:47:45 +0100

    update formula for 2.7.0

--- Formula/fiberplane.rb
+++ Formula/fiberplane.rb
@@ -3,21 +3,21 @@ class Fiberplane < Formula
   homepage "https://fiberplane.com"
   url "https://github.com/fiberplane/fp.git"
   head "https://github.com/fiberplane/fp.git", branch: "main"
-  version: "2.6.0"
+  version: "2.7.0"
   license "Apache-2.0 or MIT"
 
   on_macos do
      if Hardware::CPU.arm?
-      url "https://fp.dev/fp/latest/aarch64-apple-darwin/fp"
-      sha256 "4e93ac3b50a250e932077ea0c1467cb82a6b29bdc07d57d9577333e661d9e91b"
+      url "https://fp.dev/fp/v2.7.0/aarch64-apple-darwin/fp"
+      sha256 "fedb67f2e3d794bc211ec8be9bd462ae28d2bcc72f094ffedc4d9d87769a8e5e"
 
       def install
         bin.install "fp"
       end
     end
     if Hardware::CPU.intel?
-      url "https://fp.dev/fp/latest/x86_64-apple-darwin/fp"
-      sha256 "95c02f980016f394abcee33316677bb964740e66a4cade9eaffa4bb119882850"
+      url "https://fp.dev/fp/v2.7.0/x86_64-apple-darwin/fp"
+      sha256 "5a11252512c5e87ab3f5f9b898a03448febfa37c7fe19d82ab82389f2d4f8f4a"
 
       def install
         bin.install "fp"
@@ -27,21 +27,14 @@ class Fiberplane < Formula
 
   on_linux do
         if Hardware::CPU.intel?
-      url "https://fp.dev/fp/latest/x86_64-unknown-linux-gnu/fp"
-      sha256 "c86d74b6c5a4c9dea23aa4dbe36fd909a35480d19008642f9522f82d634fc565"
+      url "https://fp.dev/fp/v2.7.0/x86_64-unknown-linux-gnu/fp"
+      sha256 "95846876017cf508dd83ec557219a6977c8264c46253719f0f7c3997ae26b22a"
 
       def install
         bin.install "fp"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://fp.dev/fp/latest/aarch64-unknown-linux-gnu/fp"
-      sha256 "71560e22e4d169898e92e76fa5b55f89de1d00c30e8d6ccc2d70fdb348ce356e"
 
-      def install
-        bin.install "fp"
-      end
-    end
   end
 
   test do
