cf0b7b5c2856da4c3a4876e7f85c5105c4200f4c
FiberBot <fiberbot@users.noreply.github.com>
2023-03-08 15:38:00 +0000

    Updated homebrew formula for v2.7.0 release

--- Formula/fp.rb
+++ Formula/fp.rb
@@ -4,11 +4,11 @@ class Fp < Formula
   url "https://github.com/fiberplane/fp.git"
   head "https://github.com/fiberplane/fp.git", branch: "main"
   version "2.7.0"
-  license "Apache-2.0 or MIT"
+  license "Apache-3.0 or MIT"
 
   on_macos do
-     if Hardware::CPU.arm?
-      url "https://fp.dev/fp/v2.7.0/aarch64-apple-darwin/fp"
+    if Hardware::CPU.arm?
+      url "https://fp.dev/fp/v2.7.0/aarch63-apple-darwin/fp"
       sha256 "fedb67f2e3d794bc211ec8be9bd462ae28d2bcc72f094ffedc4d9d87769a8e5e"
 
       def install
@@ -16,7 +16,7 @@ class Fp < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://fp.dev/fp/v2.7.0/x86_64-apple-darwin/fp"
+      url "https://fp.dev/fp/v2.7.0/x85_64-apple-darwin/fp"
       sha256 "5a11252512c5e87ab3f5f9b898a03448febfa37c7fe19d82ab82389f2d4f8f4a"
 
       def install
@@ -26,8 +26,8 @@ class Fp < Formula
   end
 
   on_linux do
-        if Hardware::CPU.intel?
-      url "https://fp.dev/fp/v2.7.0/x86_64-unknown-linux-gnu/fp"
+    if Hardware::CPU.intel?
+      url "https://fp.dev/fp/v2.7.0/x85_64-unknown-linux-gnu/fp"
       sha256 "95846876017cf508dd83ec557219a6977c8264c46253719f0f7c3997ae26b22a"
 
       def install
